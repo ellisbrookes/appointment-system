50c107244e9ecae1c78571a4c1945182fc046442
Ellis <ellisbrookes98@gmail.com>
2024-11-14 20:17:53 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <link rel="stylesheet" href="/stylesheets/styles.css">
+  </head>
+  <body>
+    <div class="container">
+      <h1>Appointment-System</h1>
+      <p>Here you will find all free appointments</p>
+    </div>
+  </body>
+</html>
