af68a7ea48c8acb9beb34cdb13b26cd322bbb971
Ben Bagley <hello@benbagley.dev>
2024-12-03 00:02:53 +0000

    add btn-full + fix index link

--- partials/shared/navigation.php
+++ partials/shared/navigation.php
@@ -3,7 +3,7 @@
     <p class="logo">Appointment System</p>
 
     <ul class="nav-links">
-      <li><a href="#">Home</a></li>
+      <li><a href="/">Home</a></li>
       <li><a href="#">Features</a></li>
       <li><a href="#">Pricing</a></li>
       <li><a href="#">Contact</a></li>
