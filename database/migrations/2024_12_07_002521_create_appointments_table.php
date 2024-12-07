c6b3e5d38468cbc0e8788b2876e34359bb64033f
Ben Bagley <hello@benbagley.dev>
2024-12-07 04:00:25 +0000

    prettier P1

--- database/migrations/2024_12_07_002521_create_appointments_table.php
+++ database/migrations/2024_12_07_002521_create_appointments_table.php
@@ -4,23 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('appointments', function (Blueprint $table) {
+        Schema::create("appointments", function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->string('service');
-            $table->date('date');
-            $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')
-                  ->references('id')
-                  ->on('users')
-                  ->onDelete('cascade');
+            $table->string("service");
+            $table->date("date");
+            $table->unsignedBigInteger("user_id");
+            $table
+                ->foreign("user_id")
+                ->references("id")
+                ->on("users")
+                ->onDelete("cascade");
         });
     }
 
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('appointments');
+        Schema::dropIfExists("appointments");
     }
 };
