dafe591284dbd0b819cd5f2d52b9e76765d8fde4
Ellis <ellisbrookes98@gmail.com>
2024-12-07 00:31:05 +0000

    more migration changes

--- database/migrations/2024_12_07_002521_create_appointments_table.php
+++ database/migrations/2024_12_07_002521_create_appointments_table.php
@@ -14,6 +14,13 @@ return new class extends Migration
         Schema::create('appointments', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('service');
+            $table->date('date');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')
+                  ->references('id')
+                  ->on('users')
+                  ->onDelete('cascade');
         });
     }
 
