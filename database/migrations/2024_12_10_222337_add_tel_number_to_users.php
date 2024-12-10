79e487fd21fef3b6bfa9c06227b0d14647253876
Ben Bagley <hello@benbagley.dev>
2024-12-10 22:30:24 +0000

    new laravel

--- database/migrations/2024_12_10_222337_add_tel_number_to_users.php
+++ database/migrations/2024_12_10_222337_add_tel_number_to_users.php
@@ -4,14 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::table("users", function (Blueprint $table) {
-            $table->string("tel_number");
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('tel_number');
         });
     }
 
@@ -20,8 +21,8 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::table("users", function (Blueprint $table) {
-            //
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('tel_number');
         });
     }
 };
