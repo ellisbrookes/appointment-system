62d04694c548a9e0e00a7577a4491764394281d5
Ben John Bagley <hello@benbagley.dev>
2024-12-13 02:19:21 +0000

    cleanup P1

--- routes/web.php
+++ routes/web.php
@@ -17,31 +17,6 @@ Route::get('/dashboard', function() {
     return view('dashboard.index');
 });
 
-Route::get('/dashboard/appointments', function() {
-    $currentDate = Carbon::now();
-    $daysInMonth = $currentDate->daysInMonth;
-    $firstDayOfMonth = $currentDate->startOfMonth()->dayOfWeek;
-
-    // Default selected day is today
-    $selectedDay = $currentDate->day;
-
-    // If a selected day is passed in the query, update the selected day
-    if (request()->has('selected_day')) {
-        $selectedDay = request()->get('selected_day');
-    }
-
-    // Get the full date for the selected day
-    $fullDate = Carbon::create($currentDate->year, $currentDate->month, $selectedDay)->format('l, F j, Y');
-
-    // Pass the data to the view
-    return view('dashboard.appointments.index', [
-        'currentDate' => $currentDate,
-        'daysInMonth' => $daysInMonth,
-        'firstDayOfMonth' => $firstDayOfMonth,
-        'selectedDay' => $selectedDay,
-        'fullDate' => $fullDate,
-    ]);
-});
-
-// New route for the appointments view handled by AppointmentController
-Route::get('/appointments', [AppointmentController::class, 'showAppointments'])->name('appointments.view');
+Route::get('/appointments', [
+    AppointmentController::class, 'index'
+])->name('appointments.view');
