ead04954ed801e08d09ee6b6f58acbf06ffb743e
Ben John Bagley <hello@benbagley.dev>
2024-12-18 01:05:23 +0000

    Installed Laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,35 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use Carbon\Carbon;
-use App\Http\Controllers\AppointmentController;
 
-// Existing routes
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test', function () {
-    return view('test-page');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/dashboard', function() {
-    return view('dashboard.index');
-});
-
-Route::controller(AppointmentController::class)->group(function() {
-    // Appointments index route
-    Route::get('/dashboard/appointments', 'index')->name('dashboard.appointments.index');
-
-    // Step one
-    Route::get('/dashboard/appointments/create-step-one', 'createStepOne')->name('dashboard.appointments.create.step.one');
-    Route::post('/dashboard/appointments/create-step-one', 'createPostStepOne')->name('dashboard.appointments.create.step.one.post');
-
-    // Step two
-    Route::get('/dashboard/appointments/create-step-two', 'createStepTwo')->name('dashboard.appointments.create.step.two');
-    Route::post('/dashboard/appointments/create-step-two', 'createPostStepTwo')->name('dashboard.appointments.create.step.two.post');
-
-    // Step three
-    Route::get('/dashboard/appointments/create-step-three', 'createStepThree')->name('dashboard.appointments.create.step.three');
-    Route::post('/dashboard/appointments/create-step-three', 'createPostStepThree')->name('dashboard.appointments.create.step.three.post');
-});
+require __DIR__.'/auth.php';
