51beed9d65b982dcd84803223c1922a32dd0da5a
Ben John Bagley <hello@benbagley.dev>
2024-12-28 20:34:12 +0000

    Laravel auth (#16)
    * Installed Laravel breeze
    
    * readded user_id to appointments table
    
    * adeed tel number field on register page and sorted register user controller and sorted routes
    
    * replaced user name with actual user logged in
    
    * logout link added in navigation and clean up
    
    * reverts signout link on navigation
    
    * sorted Indentation out
    
    * fixed signout position
    
    * user now renders on appointment index table
    
    * cleaned up migrations - drop DB
    
    * migrations
    
    * fixed user rendering
    
    * added admin role
    
    ---------
    
    Co-authored-by: Ellis <ellisbrookes98@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use Carbon\Carbon;
 use App\Http\Controllers\AppointmentController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 
-// Existing routes
 Route::get('/', function () {
     return view('welcome');
 });
@@ -13,23 +12,28 @@ Route::get('/test', function () {
     return view('test-page');
 });
 
-Route::get('/dashboard', function() {
-    return view('dashboard.index');
+Route::middleware('auth')->group(function () {
+    Route::controller(ProfileController::class)->group(function () {
+        Route::get('/profile', 'edit')->name('profile.edit');
+        Route::patch('/profile', 'update')->name('profile.update');
+        Route::delete('/profile', 'destroy')->name('profile.destroy');
+    });
+
+    Route::get('/dashboard', function () {
+        return view('dashboard.index');
+    })->middleware('verified')->name('dashboard');
+
+    Route::prefix('dashboard/appointments')->name('dashboard.appointments.')->group(function () {
+        Route::controller(AppointmentController::class)->group(function () {
+            Route::get('/', 'index')->name('index');
+            Route::get('/create-step-one', 'createStepOne')->name('create.step.one');
+            Route::post('/create-step-one', 'createPostStepOne')->name('create.step.one.post');
+            Route::get('/create-step-two', 'createStepTwo')->name('create.step.two');
+            Route::post('/create-step-two', 'createPostStepTwo')->name('create.step.two.post');
+            Route::get('/create-step-three', 'createStepThree')->name('create.step.three');
+            Route::post('/create-step-three', 'createPostStepThree')->name('create.step.three.post');
+        });
+    });
 });
 
-Route::controller(AppointmentController::class)->group(function() {
-    // Appointments index route
-    Route::get('/dashboard/appointments', 'index')->name('dashboard.appointments.index');
-
-    // Step one
-    Route::get('/dashboard/appointments/create-step-one', 'createStepOne')->name('dashboard.appointments.create.step.one');
-    Route::post('/dashboard/appointments/create-step-one', 'createPostStepOne')->name('dashboard.appointments.create.step.one.post');
-
-    // Step two
-    Route::get('/dashboard/appointments/create-step-two', 'createStepTwo')->name('dashboard.appointments.create.step.two');
-    Route::post('/dashboard/appointments/create-step-two', 'createPostStepTwo')->name('dashboard.appointments.create.step.two.post');
-
-    // Step three
-    Route::get('/dashboard/appointments/create-step-three', 'createStepThree')->name('dashboard.appointments.create.step.three');
-    Route::post('/dashboard/appointments/create-step-three', 'createPostStepThree')->name('dashboard.appointments.create.step.three.post');
-});
+require __DIR__.'/auth.php';
