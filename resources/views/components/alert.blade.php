5f85bc5ebe9a2fed8e48f18410e797594a37d670
Ben John Bagley <hello@benbagley.dev>
2024-12-30 02:42:40 +0000

    alerts cleanup

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,30 +1,5 @@
-@props(['type'])
-
-@switch($type)
-    @case('success')
-        <div class="border-l-4 p-4 bg-green-100 text-green-800 border-green-300">
-            <p class="font-medium">
-                {{ $slot }}
-            </p>
-        </div>
-        @break
-
-    @case('danger')
-        <div class="border-l-4 p-4 bg-red-100 text-red-800 border-red-300">
-            <p class="font-medium">
-                {{ $slot }}
-            </p>
-        </div>
-        @break
-
-    @case('warning')
-        <div class="border-l-4 p-4 bg-yellow-100 text-yellow-800 border-yellow-300">
-            <p class="font-medium">
-                {{ $slot }}
-            </p>
-        </div>
-        @break
-
-    @default
-        Default case...
-@endswitch
+<div {{ $attributes->merge(['class' => 'border-l-4 p-4 my-2 ' . $alertVariant]) }}>
+    <p class="font-medium">
+        {{ $slot }}
+    </p>
+</div>
