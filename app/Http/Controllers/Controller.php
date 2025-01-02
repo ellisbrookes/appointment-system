bb2442ee424a2bb5a19c7043ca8e36e4f016db91
Ellis <ellisbrookes98@gmail.com>
2025-01-02 19:53:31 +0000

    pricing table test

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use Stripe\StripeClient;
+
 abstract class Controller
 {
-    //
+    protected $stripe;
+
+    public function __construct()
+    {
+        $this->stripe = new StripeClient(env('STRIPE_SECRET'));
+    }
+
+    public function index()
+    {
+        try {
+            $products = $this->stripe->products->all(['active' => true]);
+            $prices - $this->stripe->prices->all(['active' => true]);
+
+            $productsWithPrices = collect($products->data)->map(function ($product) use ($price) {
+                $productPrices = collect($prices->data)->filter(function ($price) use ($product) {
+                    return $price->product === $product->id;
+                });
+
+                $product->prices = $productPrices;
+                return $product;
+            });
+
+            return view('products.index', compact('productsWithPrices'));
+        } catch (\Expection $e) {
+            return back()->withErrors(['error' => $e->getMessage()]);
+        }
+    }
 }
