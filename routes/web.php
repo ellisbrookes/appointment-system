63b768c3d9dcf80b8e414158d9d9564dd5fedbb1
Ellis Marcus Brookes <ellisbrookes98@gmail.com>
2025-03-19 16:41:53 +0000

    Billing Setup (#19)
    * installed stripe
    
    * remove stripe/stripe-php moving to laravel/cahsier
    
    * migrations added
    
    * Basic stripe checkout setup
    
    * pricing table test
    
    * pricing table WIP
    
    * package changes and added yearly and monthly toggle to pricing page
    
    * subscription banner WIP
    
    * testing
    
    * added billing area
    
    * style changes
    
    * package updates
    
    * package updates
    
    ---------
    
    Co-authored-by: Ellis Brookes <ellis@Ellis-Laptop>
    Co-authored-by: root <root@Ellis-Laptop>

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,25 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AppointmentController;
-use App\Http\Controllers\EmailController;
+use App\Http\Controllers\PricingController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\CheckSubscription;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
 Route::get('/test', function () {
     return view('test-page');
-});
+})->name('test');
+
+Route::get('/pricing', [PricingController::class, 'index'])->name('pricing');
 
 Route::middleware('auth')->group(function () {
     Route::controller(ProfileController::class)->group(function () {
+
         Route::get('/profile', 'edit')->name('profile.edit');
         Route::patch('/profile', 'update')->name('profile.update');
         Route::delete('/profile', 'destroy')->name('profile.destroy');
@@ -22,7 +27,22 @@ Route::middleware('auth')->group(function () {
 
     Route::get('/dashboard', function () {
         return view('dashboard.index');
-    })->middleware('verified')->name('dashboard');
+    })->middleware(['verified', CheckSubscription::class])->name('dashboard');
+
+    Route::get('/billing', function (Request $request) {
+        return $request->user()->redirectToBillingPortal(route('dashboard'));;
+    })->name('billing');
+
+
+Route::post('/subscription-checkout', function (Request $request) {
+    return $request->user()
+        ->newSubscription('basic', 'price_1QbtKfGVcskF822y3QlF13vZ')
+        ->allowPromotionCodes()
+        ->checkout([
+            'success_url' => route('dashboard'),
+            'cancel_url' => route('welcome'),
+        ]);
+    })->name('subscription');
 
     Route::prefix('dashboard/appointments')->name('dashboard.appointments.')->group(function () {
         Route::controller(AppointmentController::class)->group(function () {
