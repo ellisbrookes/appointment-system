7ed384b69c5bfe918a6ba8399d0781d41b657542
Ellis Marcus Brookes <ellisbrookes98@gmail.com>
2025-03-19 16:41:53 +0000

    Billing Setup (#19)
    * installed stripe
    
    * remove stripe/stripe-php moving to laravel/cahsier
    
    * migrations added
    
    * Basic stripe checkout setup
    
    * pricing table test
    
    * pricing table WIP
    
    * package changes and added yearly and monthly toggle to pricing page
    
    * subscription banner WIP
    
    * testing
    
    * added billing area
    
    * style changes
    
    * package updates
    
    * package updates
    
    ---------
    
    Co-authored-by: Ellis Brookes <ellis@Ellis-Laptop>
    Co-authored-by: root <root@Ellis-Laptop>

---
+++ app/Http/Middleware/CheckSubscription.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckSubscription
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = $request->user();
+
+        // Define excluded routes
+        $excludedRoutes = ['pricing', 'dashboard'];
+
+        // Skip the middleware check for excluded routes
+        if (in_array($request->route()->getName(), $excludedRoutes)) {
+            return $next($request);
+        }
+
+        // Check subscription status
+        if ($user && $user->subscribed('basic')) {
+            return $next($request);
+        }
+
+        // Redirect to pricing page if the subscription is inactive
+        return redirect()->route('pricing')->with('alert', [
+            'type' => 'danger',
+            'message' => 'Your subscription is inactive. Please renew to continue.',
+        ]);
+    }
+}
