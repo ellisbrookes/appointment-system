2868dd8462543fc81add0a11b2d9c9d27ed6aaaf
Ben Bagley <hello@benbagley.dev>
2025-05-23 20:37:40 +0100

    Run org and ana code

--- resources/views/dashboard/appointments/create-step-three.blade.php
+++ resources/views/dashboard/appointments/create-step-three.blade.php
@@ -1,3 +1,4 @@
+@php use Carbon\Carbon; @endphp
 @extends('partials.dashboard.layout')
 
 @section('content')
@@ -12,51 +13,55 @@
                 <div class="overflow-x-auto">
                     <table class="w-full border border-gray-200 rounded-lg text-sm">
                         <thead>
-                            <tr class="bg-gray-800 text-white uppercase text-left">
-                                <th class="px-6 py-3">Information</th>
-                                <th class="px-6 py-3">Details</th>
-                            </tr>
+                        <tr class="bg-gray-800 text-white uppercase text-left">
+                            <th class="px-6 py-3">Information</th>
+                            <th class="px-6 py-3">Details</th>
+                        </tr>
                         </thead>
                         <tbody class="text-gray-700 dark:text-gray-300">
-                            <tr class="hover:bg-gray-50 transition duration-200">
-                                <td class="border px-6 py-4 font-medium">Service</td>
-                                <td class="border px-6 py-4">{{ $appointment['service'] ?? '' }}</td>
-                            </tr>
-                            <tr class="hover:bg-gray-50 transition duration-200">
-                                <td class="border px-6 py-4 font-medium">Date</td>
-                                <td class="border px-6 py-4">
-                                    {{ \Carbon\Carbon::parse($appointment['date'])->format('d/m/Y') ?? '' }}
-                                </td>
-                            </tr>
-                            <tr class="hover:bg-gray-50 transition duration-200">
-                                <td class="border px-6 py-4 font-medium">Time</td>
-                                <td class="border px-6 py-4">{{ $appointment['timeslot'] ?? '' }}</td>
-                            </tr>
-                            <tr class="hover:bg-gray-50 transition duration-200">
-                                <td class="border px-6 py-4 font-medium">User</td>
-                                <td class="border px-6 py-4">
-                                    @if (Auth::user()->admin)
-                                        <select name="user_id" id="user_id" class="block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 text-sm">
-                                            <option value="" disabled selected>Select a user</option>
-                                            @foreach ($users as $user)
-                                                <option value="{{ $user->id }}">{{ $user->name }}</option>
-                                            @endforeach
-                                        </select>
-                                    @else
-                                        {{ Auth::user()->name }}
-                                        <input type="hidden" name="user_id" id="user_id" value="{{ Auth::user()->id }}">
-                                    @endif
-                                </td>
-                            </tr>
+                        <tr class="hover:bg-gray-50 transition duration-200">
+                            <td class="border px-6 py-4 font-medium">Service</td>
+                            <td class="border px-6 py-4">{{ $appointment['service'] ?? '' }}</td>
+                        </tr>
+                        <tr class="hover:bg-gray-50 transition duration-200">
+                            <td class="border px-6 py-4 font-medium">Date</td>
+                            <td class="border px-6 py-4">
+                                {{ Carbon::parse($appointment['date'])->format('d/m/Y') ?? '' }}
+                            </td>
+                        </tr>
+                        <tr class="hover:bg-gray-50 transition duration-200">
+                            <td class="border px-6 py-4 font-medium">Time</td>
+                            <td class="border px-6 py-4">{{ $appointment['timeslot'] ?? '' }}</td>
+                        </tr>
+                        <tr class="hover:bg-gray-50 transition duration-200">
+                            <td class="border px-6 py-4 font-medium">User</td>
+                            <td class="border px-6 py-4">
+                                @if (Auth::user()->admin)
+                                    <select name="user_id" id="user_id"
+                                            class="block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 text-sm">
+                                        <option value="" disabled selected>Select a user</option>
+                                        @foreach ($users as $user)
+                                            <option value="{{ $user->id }}">{{ $user->name }}</option>
+                                        @endforeach
+                                    </select>
+                                @else
+                                    {{ Auth::user()->name }}
+                                    <input type="hidden" name="user_id" id="user_id" value="{{ Auth::user()->id }}">
+                                @endif
+                            </td>
+                        </tr>
                         </tbody>
                     </table>
                 </div>
 
                 <!-- Previous and Submit Buttons -->
                 <div class="flex justify-between mt-4">
-                    <a href="{{ route('dashboard.appointments.create.step.two') }}" class="bg-gray-600 text-white py-3 px-4 rounded-md hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50 transition duration-200">Previous Step</a>
+                    <a href="{{ route('dashboard.appointments.create.step.two') }}"
+                       class="bg-gray-600 text-white py-3 px-4 rounded-md hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50 transition duration-200">Previous
+                        Step</a>
 
-                    <button type="submit" class="bg-blue-600 text-white py-3 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-200">
+                    <button type="submit"
+                            class="bg-blue-600 text-white py-3 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-200">
                         Confirm
                     </button>
                 </div>
--- resources/views/dashboard/appointments/index.blade.php
+++ resources/views/dashboard/appointments/index.blade.php
@@ -1,3 +1,4 @@
+@php use Carbon\Carbon; @endphp
 @extends('partials.dashboard.layout')
 
 @section('content')
@@ -5,7 +6,7 @@
         <div class="flex justify-between items-center mb-6">
             <h2 class="text-4xl font-bold text-center text-gray-700 dark:text-white">Appointments List</h2>
             <a href="{{ route('dashboard.appointments.create.step.one') }}"
-                class="bg-green-600 text-white dark:text-white px-6 py-3 rounded-lg hover:bg-green-700 transition duration-200">
+               class="bg-green-600 text-white dark:text-white px-6 py-3 rounded-lg hover:bg-green-700 transition duration-200">
                 Book Appointment
             </a>
         </div>
@@ -16,44 +17,49 @@
             <div class="overflow-x-auto">
                 <table class="w-full border border-gray-400 border-collapse text-md table-auto md:table-fixed text-center">
                     <thead>
-                        <tr class="bg-gray-200 dark:bg-gray-800 text-gray-500 dark:text-white">
-                            <th class="px-6 py-3 border border-gray-300 text-center">Service</th>
-                            <th class="px-6 py-3 border border-gray-300 text-center">Date</th>
-                            <th class="px-6 py-3 border border-gray-300 text-center">User</th>
-                            <th class="px-6 py-3 border border-gray-300 text-center">Timeslot</th>
-                            <th class="px-6 py-3 border border-gray-300 text-center">Actions</th>
-                        </tr>
+                    <tr class="bg-gray-200 dark:bg-gray-800 text-gray-500 dark:text-white">
+                        <th class="px-6 py-3 border border-gray-300 text-center">Service</th>
+                        <th class="px-6 py-3 border border-gray-300 text-center">Date</th>
+                        <th class="px-6 py-3 border border-gray-300 text-center">User</th>
+                        <th class="px-6 py-3 border border-gray-300 text-center">Timeslot</th>
+                        <th class="px-6 py-3 border border-gray-300 text-center">Actions</th>
+                    </tr>
                     </thead>
                     <tbody>
-                        @foreach ($appointments as $appointment)
-                            <tr class="even:bg-gray-50 dark:even:bg-gray-900 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-900">
-                                <td class="px-6 py-4 border border-gray-300">{{ $appointment->service }}</td>
-                                <td class="px-6 py-4 border border-gray-300">
-                                    {{ \Carbon\Carbon::parse($appointment->date)->format('jS F Y') }}
-                                </td>
-                                <td class="px-6 py-4 border border-gray-300">
-                                    {{ $appointment->user->name ?? 'Guest' }}
-                                </td>
-                                <td class="px-6 py-4 border border-gray-300">
-                                    {{ \Carbon\Carbon::parse($appointment->timeslot)->format('g:i A') }}
-                                </td>
-                                <td class="px-6 py-4 border-b border-gray-300">
-                                    <div class="flex justify-center space-x-2">
-                                        <a href="{{ route('dashboard.appointments.edit', $appointment->id) }}" class="bg-blue-600 text-white py-2 px-4 flex align-center rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-200">
-                                            Edit
-                                        </a>
+                    @foreach ($appointments as $appointment)
+                        <tr class="even:bg-gray-50 dark:even:bg-gray-900 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-900">
+                            <td class="px-6 py-4 border border-gray-300">{{ $appointment->service }}</td>
+                            <td class="px-6 py-4 border border-gray-300">
+                                {{ Carbon::parse($appointment->date)->format('jS F Y') }}
+                            </td>
+                            <td class="px-6 py-4 border border-gray-300">
+                                {{ $appointment->user->name ?? 'Guest' }}
+                            </td>
+                            <td class="px-6 py-4 border border-gray-300">
+                                {{ Carbon::parse($appointment->timeslot)->format('g:i A') }}
+                            </td>
+                            <td class="px-6 py-4 border-b border-gray-300">
+                                <div class="flex justify-center space-x-2">
+                                    <a href="{{ route('dashboard.appointments.edit', $appointment->id) }}"
+                                       class="bg-blue-600 text-white py-2 px-4 flex align-center rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-200">
+                                        Edit
+                                    </a>
 
-                                        <form method="POST" action="{{ route('dashboard.appointments.destroy', $appointment->id) }}" onsubmit="return confirm('Are you sure you want to cancel this appointment?');" style="display: inline;">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit" class="bg-red-600 text-white py-2 px-4 flex items-center rounded-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50 transition duration-200">
-                                                Cancel
-                                            </button>
-                                        </form>
-                                    </div>
-                                </td>
-                            </tr>
-                        @endforeach
+                                    <form method="POST"
+                                          action="{{ route('dashboard.appointments.destroy', $appointment->id) }}"
+                                          onsubmit="return confirm('Are you sure you want to cancel this appointment?');"
+                                          style="display: inline;">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit"
+                                                class="bg-red-600 text-white py-2 px-4 flex items-center rounded-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50 transition duration-200">
+                                            Cancel
+                                        </button>
+                                    </form>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
                     </tbody>
                 </table>
             </div>
