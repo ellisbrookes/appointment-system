4b22f7f1453b9965e61c2f23beeef6f3b744a71b
Ben Bagley <hello@benbagley.dev>
2025-05-25 11:31:36 +0100

    component cleanup

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,5 +1,5 @@
 <div {{ $attributes->merge(['class' => 'border-l-4 p-4 my-2 ' . $alertVariant]) }}>
-    <p class="font-medium">
-        {{ $slot }}
-    </p>
+  <p class="font-medium">
+    {{ $slot }}
+  </p>
 </div>
--- resources/views/dashboard/profile/edit.blade.php
+++ resources/views/dashboard/profile/edit.blade.php
@@ -1,32 +1,29 @@
 @extends('layouts.dashboard')
 
 @section('content')
+  <div class="py-12">
+    <div class="max-w-7xl mx-auto space-y-6">
+      <h2 class="font-semibold text-2xl leading-tight">
+        {{ __('Edit profile') }}
+      </h2>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto space-y-6">
-            <h2 class="font-semibold text-2xl text-black dark:text-white leading-tight">
-                {{ __('Edit profile') }}
-            </h2>
-
-            <div class="p-4 sm:p-8 bg-white border border-gray-400 dark:bg-gray-900 sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('dashboard.profile.partials.update-profile-information-form')
-                </div>
-            </div>
-
-            <div class="p-4 sm:p-8 bg-white border border-gray-400 dark:bg-gray-900 sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('dashboard.profile.partials.update-password-form')
-                </div>
-            </div>
-
-            <div class="p-4 sm:p-8 bg-white border border-gray-400 dark:bg-gray-900 sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('dashboard.profile.partials.delete-user-form')
-                </div>
-            </div>
+      <div class="p-4 sm:p-8 bg-white border dark:bg-gray-900 sm:rounded-lg">
+        <div class="max-w-xl">
+          @include('dashboard.profile.partials.update-profile-information-form')
         </div>
-    </div>
-    </div>
+      </div>
 
+      <div class="p-4 sm:p-8 bg-white border dark:bg-gray-900 sm:rounded-lg">
+        <div class="max-w-xl">
+          @include('dashboard.profile.partials.update-password-form')
+        </div>
+      </div>
+
+      <div class="p-4 sm:p-8 bg-white border dark:bg-gray-900 sm:rounded-lg">
+        <div class="max-w-xl">
+          @include('dashboard.profile.partials.delete-user-form')
+        </div>
+      </div>
+    </div>
+  </div>
 @endsection
