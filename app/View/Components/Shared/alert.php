f6fd5e1626d33666d5e1e7b1fd1da0215d7bf3d0
Ben Bagley <hello@benbagley.dev>
2025-05-25 15:18:01 +0100

    rename shared to Shared

--- app/View/Components/Shared/alert.php
+++ app/View/Components/Shared/alert.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\View\Components;
+namespace App\View\Components\Shared;
 
 use Closure;
 use Illuminate\Contracts\View\View;
--- resources/views/dashboard/layout.blade.php
+++ resources/views/dashboard/layout.blade.php
@@ -17,25 +17,12 @@
 
   <main :class="isSidebarOpen ? 'ml-64' : 'ml-16'" class="flex flex-col flex-1 py-8 px-6 bg-white dark:bg-gray-900 h-screen">
     @if (session('alert'))
-      <x-alert :type="session('alert')['type']">
+      <x-shared.alert :type="session('alert')['type']">
         {{ session('alert')['message'] }}
-      </x-alert>
+      </x-shared.alert>
     @endif
 
     @yield('content')
   </main>
-
-  <!-- Blur Overlay -->
-  <!-- @if(auth()->user()->subscribed('basic') === false)
-    <div class="fixed inset-0 bg-black/70 flex items-center justify-center z-50">
-      <div class="bg-white dark:bg-slate-800 rounded-lg p-6 max-w-lg mx-auto text-center shadow-lg">
-        <h1 class="text-2xl font-semibold text-gray-800 dark:text-white mb-2">Your subscription is inactive</h1>
-        <p class="mb-4 text-sm">Please renew your subscription to continue using the app.</p>
-        <a href="{{ route('billing') }}" class="bg-blue-600 hover:bg-blue-700 text-white text-sm px-6 py-2 rounded-md transition duration-200">
-          Renew Subscription
-        </a>
-      </div>
-    </div>
-  @endif -->
 </body>
 </html>
