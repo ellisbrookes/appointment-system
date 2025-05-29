0c45971a88a3652150fed4d545a8d994007f4c81
Ellis Marcus Brookes <ellisbrookes98@gmail.com>
2025-05-29 01:34:00 +0100

    Auth P2 - Reset Password  (#24)

--- routes/auth.php
+++ routes/auth.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\SessionController;
+use App\Http\Controllers\Auth\PasswordForgotController;
+use App\Http\Controllers\Auth\PasswordResetController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function () {
@@ -12,6 +14,14 @@ Route::prefix('auth')->group(function () {
   // Login
   Route::get('login', [SessionController::class, 'create'])->name('login');
   Route::post('login', [SessionController::class, 'store']);
+
+  // Forgot Password
+  Route::get('forgot-password', [PasswordForgotController::class, 'create'])->name('password.request');
+  Route::post('forgot-password', [PasswordForgotController::class, 'store'])->name('password.email');
+
+  // Reset Password
+  Route::get('reset-password/{token}', [PasswordResetController::class, 'create'])->name('password.reset');
+  Route::post('reset-password', [PasswordResetController::class, 'store'])->name('password.store');
 
   // Logout
   Route::post('logout', [SessionController::class, 'destroy'])->name('logout');
