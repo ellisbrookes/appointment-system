c5d551c8c1a338946aa69ab27d0a8b2198aabc0b
Ellis <ellisbrookes98@gmail.com>
2025-05-29 01:23:56 +0100

    password reset flow

---
+++ resources/views/auth/reset-password.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.layout')
+
+@section('content')
+  <div class="flex flex-col md:flex-row md:items-center space-y-4 md:space-y-0 h-full p-4">
+    <x-shared.header type="sidebar" heading="Reset Password" subheading="Use this page to reset your password" />
+
+    <div class="md:flex md:justify-center md:items-center h-full md:w-full">
+      <form method="POST" action="{{ route('password.store') }}" class="flex flex-col space-y-4 p-16 rounded-md border">
+        @csrf
+
+        <input type="hidden" name="token" value="{{ $request->route('token') }}"/>
+
+        <!-- Email address -->
+        <div>
+          <x-shared.input-label for="email" :value="__('Email')" />
+          <x-shared.text-input id="email" type="email" name="email" :value="old('email', $request->email)" required />
+          
+          <x-shared.input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
+
+        <!-- Password -->
+        <div>
+          <x-shared.input-label for="password" :value="__('Password')" />
+          <x-shared.text-input id="password" type="password" name="password" required />
+
+          <x-shared.input-error :messages="$errors->get('password')" class="mt-2" />
+        </div>
+
+        <!-- Confirm password -->
+        <div>
+          <x-shared.input-label for="password_confirmation" :value="__('Confirm Password')" />
+          <x-shared.text-input id="password_confirmation" type="password" name="password_confirmation" required />
+
+          <x-shared.input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+        </div>
+
+        <div class="flex items-center justify-end">
+          <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-hidden focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('dashboard') }}">
+            {{ __('Know your password?') }}
+          </a>
+
+          <x-shared.primary-button class="ms-4">
+            {{ __('Submit') }}
+          </x-shared.primary-button>
+        </div>
+      </form>
+    </div>
+  </div>
+@endsection
