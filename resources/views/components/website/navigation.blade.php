9113fe81ddf9c4f5be338f0af99a737f0e663f87
Ellis <ellisbrookes98@gmail.com>
2025-05-31 03:39:16 +0100

    dark mode added on auth pages

--- resources/views/components/website/navigation.blade.php
+++ resources/views/components/website/navigation.blade.php
@@ -1,12 +1,12 @@
-<nav class="bg-gray-800 text-white py-4 w-full shadow-lg rounded-b-lg">
+<nav class="dark:bg-gray-800 text-white py-4 w-full border-b border-gray-300 dark:border-gray-700">
   <div class="container mx-auto flex justify-between items-center">
-    <p class="logo text-xl font-bold text-white">Appointment System</p>
+    <p class="logo text-xl font-bold dark:text-white">Appointment System</p>
 
     <ul class="flex gap-6">
-      <li><a href="/" class="hover:text-blue-400 transition-colors">Home</a></li>
-      <li><a href="#" class="hover:text-blue-400 transition-colors">Features</a></li>
-      <li><a href="#" class="hover:text-blue-400 transition-colors">Pricing</a></li>
-      <li><a href="#" class="hover:text-blue-400 transition-colors">Contact</a></li>
+      <li><a href="/" class="hover:text-blue-400 text-black dark:text-white transition-colors">Home</a></li>
+      <li><a href="#" class="hover:text-blue-400 text-black dark:text-white transition-colors">Features</a></li>
+      <li><a href="#" class="hover:text-blue-400 text-black dark:text-white transition-colors">Pricing</a></li>
+      <li><a href="#" class="hover:text-blue-400 text-black dark:text-white transition-colors">Contact</a></li>
     </ul>
 
     <a href="/dashboard" class="bg-blue-600 text-white rounded-md border px-6 py-2 hover:bg-blue-700 transition-colors">Get Started</a>
