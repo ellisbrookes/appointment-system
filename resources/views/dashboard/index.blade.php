17e1cfbfa2668d7e5272803caa78259cda1568f8
Ellis <ellisbrookes98@gmail.com>
2025-05-31 03:08:45 +0100

    dashboard styling tweaks

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,8 +1,8 @@
 @extends('dashboard.layout')
 
 @section('content')
-  <div class="flex flex-col items-center w-full max-w-7xl mx-auto mb-2">
+  <div class="flex flex-col items-center w-full max-w-7xl mx-auto">
     <h1 class="text-4xl font-bold mb-2"> Welcome, {{ auth()->user()->name }}</h1>
-    <p class="text-lg">Here you will find all the information you need</p>
+    <p class="text-lg font-semi-bold mb-6">Here you will find all the information you need</p>
   </div>
 @endsection
