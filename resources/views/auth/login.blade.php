381524e6af0fd96ce187f52e8d7fa53744d9ec3d
Ellis <ellisbrookes98@gmail.com>
2025-06-18 21:10:36 +0100

    ran prettier

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -29,10 +29,7 @@
                         required
                     />
 
-                    <x-shared.input-error
-                        :messages="$errors->get('email')"
-                        class="mt-2"
-                    />
+                    <x-shared.input-error :messages="$errors->get('email')" />
                 </div>
 
                 <!-- Password -->
@@ -50,7 +47,6 @@
 
                     <x-shared.input-error
                         :messages="$errors->get('password')"
-                        class="mt-2"
                     />
                 </div>
 
@@ -58,19 +54,22 @@
                     <x-shared.primary-button>
                         {{ __("Login") }}
                     </x-shared.primary-button>
-                    
-        <div class="flex flex-col items-center justify-end space-y-2">
-          <x-shared.primary-button>
-            {{ __('Login') }}
-          </x-shared.primary-button>
 
-          <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-hidden focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('register') }}">
-            {{ __('Need an account?') }}
-          </a>
+                    <a
+                        class="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:outline-hidden dark:text-gray-400 dark:hover:text-gray-100 dark:focus:ring-offset-gray-800"
+                        href="{{ route("register") }}"
+                    >
+                        {{ __("Need an account?") }}
+                    </a>
 
-          <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-hidden focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-            {{ __('Forgot Password?') }}
-          </a>
+                    <a
+                        class="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:outline-hidden dark:text-gray-400 dark:hover:text-gray-100 dark:focus:ring-offset-gray-800"
+                        href="{{ route("password.request") }}"
+                    >
+                        {{ __("Forgot Password?") }}
+                    </a>
+                </div>
+            </form>
         </div>
     </div>
 @endsection
