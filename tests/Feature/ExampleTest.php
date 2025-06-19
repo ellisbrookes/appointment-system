9a43283092aac9a45aa1877955f516a9dc63a83c
Ellis <ellisbrookes98@gmail.com>
2025-06-19 01:16:05 +0100

    testing

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,7 +2,6 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
@@ -12,6 +11,11 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response(): void
     {
+        // Skip this test if the Stripe key is not set
+        if (empty(env('STRIPE_SECRET'))) {
+            $this->markTestSkipped('Skipping test: STRIPE_SECRET not set.');
+        }
+
         $response = $this->get('/');
 
         $response->assertStatus(200);
