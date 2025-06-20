2393b890c17ab1181481423377551fa0d50af6a5
Ellis <ellisbrookes98@gmail.com>
2025-06-20 17:47:12 +0100

    test changes and updates

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,25 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\File;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        if (File::exists(base_path('.env.testing.local'))) {
+            $contents = file_get_contents(base_path('.env.testing.local'));
+            foreach (explode("\n", $contents) as $line) {
+                if (!empty($line) && str_contains($line, '=')) {
+                    putenv(trim($line));
+                    [$key, $value] = explode('=', trim($line));
+                    config(['database.' . strtolower(explode('_', $key)[1]) => trim($value)]);
+                }
+            }
+        }
+    }
 }
