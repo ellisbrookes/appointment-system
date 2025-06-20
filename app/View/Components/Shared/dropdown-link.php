a4191e4570dbaea91c35f30b7d9be651913a90cf
Ellis <ellisbrookes98@gmail.com>
2025-06-20 16:50:07 +0100

    Update component class names and gitignore
    - Rename component classes to follow PHP naming conventions:
      - danger-button -> DangerButton
      - dropdown-link -> DropdownLink
      - responsive-nav-link -> ResponsiveNavLink
      - secondary-button -> SecondaryButton
    - Add /coverage to .gitignore
    - Update testing environment configuration

--- app/View/Components/Shared/dropdown-link.php
+++ app/View/Components/Shared/dropdown-link.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace App\View\Components\shared;
+namespace App\View\Components\Shared;
 
 use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class dropdown-link extends Component
+class DropdownLink extends Component
 {
     /**
      * Create a new component instance.
--- app/View/Components/Shared/responsive-nav-link.php
+++ app/View/Components/Shared/responsive-nav-link.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace App\View\Components\shared;
+namespace App\View\Components\Shared;
 
 use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class responsive-nav-link extends Component
+class ResponsiveNavLink extends Component
 {
     /**
      * Create a new component instance.
