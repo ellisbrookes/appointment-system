48003b3c6c8a86cb3eae9208e40e5921e4c9cf15
Ellis <ellisbrookes98@gmail.com>
2025-06-23 04:03:14 +0100

    Fix Laravel component naming issues
    - Fixed PHP class naming from kebab-case to PascalCase (link.php -> Link.php, modal.php -> Modal.php)
    - Fixed namespace capitalization from 'shared' to 'Shared'
    - Created missing component PHP classes for Dashboard, Website, and Shared directories
    - All components now follow Laravel naming conventions:
      * PHP classes: PascalCase
      * Blade views: kebab-case
      * Component usage: kebab-case
    - Cleared view cache to ensure changes take effect

--- app/View/Components/Shared/TextInput.php
+++ app/View/Components/Shared/TextInput.php
@@ -1,22 +1,19 @@
 <?php
 
-namespace App\View\Components\shared;
+namespace App\View\Components\Shared;
 
 use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class link extends Component
+class TextInput extends Component
 {
-    public $route;
-
     /**
      * Create a new component instance.
-     * @param $route
      */
-    public function __construct($route)
+    public function __construct()
     {
-        $this->route = $route;
+        //
     }
 
     /**
@@ -24,6 +21,6 @@ class link extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.shared.link');
+        return view('components.shared.text-input');
     }
 }
