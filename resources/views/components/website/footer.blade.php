4f1a055559a3852ac657f3c813b763e108cdc74f
Ellis Marcus Brookes <ellisbrookes98@gmail.com>
2025-06-23 02:48:31 +0100

    3 Homepage refactor (#32)

--- resources/views/components/website/footer.blade.php
+++ resources/views/components/website/footer.blade.php
@@ -1,36 +1,10 @@
 <footer>
-    <div
-        class="mt-auto border-t border-gray-300 py-4 text-center dark:border-gray-700 dark:bg-gray-800"
-    >
-        <p>
-            &copy; <?php echo date("Y"); ?> Appointment System. All rights
-            reserved.
-        </p>
-        <div class="mt-2 inline-flex">
-            <a
-                href="/"
-                class="mx-3 border-r-2 border-black pr-3 text-black transition-colors last:border-none hover:text-blue-400 dark:border-white dark:text-white"
-            >
-                Terms & Conditions
-            </a>
-            <a
-                href="/"
-                class="mx-3 border-r-2 border-black pr-3 text-black transition-colors last:border-none hover:text-blue-400 dark:border-white dark:text-white"
-            >
-                Terms of Services
-            </a>
-            <a
-                href="/"
-                class="mx-3 border-r-2 border-black pr-3 text-black transition-colors last:border-none hover:text-blue-400 dark:border-white dark:text-white"
-            >
-                Privacy Policy
-            </a>
-            <a
-                href="/"
-                class="mx-3 text-black transition-colors last:border-none hover:text-blue-400 dark:text-white"
-            >
-                Uptime Status
-            </a>
-        </div>
+  <div class="dark:bg-gray-800 text-center py-4 border-t border-gray-400 dark:border-white">
+    <p>&copy; <?php echo date("Y"); ?> Appointment System. All rights reserved.</p>
+    <div class="mt-2 inline-flex">
+      <a href="/" class="hover:underline mx-2">Terms & Conditions</a>
+      <a href="/" class="hover:underline mx-2">Terms of Services</a>
+      <a href="/" class="hover:underline mx-2">Privacy Policy</a>
+      <a href="/" class="hover:underline mx-2"> Uptime Status</a>
     </div>
 </footer>
